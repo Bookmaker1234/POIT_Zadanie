0b575f48f4eb2e25680f4d5b308a47bc4541704c
Bookmaker1234 <xkunam@stuba.sk>
2023-05-04 02:20:27 +0200

    Vypisovanie z databazy begin a end komunikacia

--- app.py
+++ app.py
@@ -32,6 +32,10 @@ def background_thread(args):
     count = 0
     dataCounter = 0
     global A
+    global D
+    global b
+    b = 2
+    D = 0
     A = 1
     dataList = []
     db = MySQLdb.connect(host=myhost,user=myuser,passwd=mypasswd,db=mydb)          
@@ -44,54 +48,69 @@ def background_thread(args):
 #               A = 100
             
         
-            if s == 1:
+            if b == 1:
                 ser = serial.Serial("/dev/ttyS0")
                 ser.baudrate = 9600
                 socketio.sleep(1)
                 count += 1
+            elif b == 0:
+                ser.close()
             while (s == 1):
-                count += 1
-                dataCounter +=1
-                read_ser = ser.readline()
-                premenna = read_ser.decode().replace("\r\n","").split(",")
-                teplota = premenna[0]
-                vlhkost = premenna[1]
-            
-                if s == 1:
-                    dataDict = {
-                        "t": time.time(),
-                        "x": dataCounter,
-                        "teplota": teplota,
-                        "vlhkost": vlhkost}
-                    dataList.append(dataDict)
-                elif s == 0:
-                    if len(dataList)>0:
-                        print(str(dataList))
-                        fuj = str(dataList).replace("'", "\"")
-                        print(fuj)
-                        fo = open("static/zapisovanie/databaza.txt","a+")
-                        fo.write("%s\r\n" %fuj)
-                        fo.close()
-                        cursor = db.cursor()
-                        cursor.execute("SELECT MAX(id) FROM merania")
-                        maxid = cursor.fetchone()
-                        cursor.execute("INSERT INTO merania (id, hodnoty) VALUES (%s, %s)", (maxid[0] + 1, fuj))
-                        db.commit()
+                if b == 2:
+                    s = 0
+                else:
+                    count += 1
+                    dataCounter +=1
+                    read_ser = ser.readline()
+                    premenna = read_ser.decode().replace("\r\n","").split(",")
+                    teplota = premenna[0]
+                    vlhkost = premenna[1]
+                    if float(A) >= float (vlhkost):
+                        kontrola = "Je tu vysoka vlhkost"
+                        
+                    if s == 1:
+                        dataDict = {
+                            "t": time.time(),
+                            "x": dataCounter,
+                            "teplota": teplota,
+                            "vlhkost": vlhkost}
+                        dataList.append(dataDict)
+                    elif s == 0:
+                        if len(dataList)>0:
+                            print(str(dataList))
+                            fuj = str(dataList).replace("'", "\"")
+                            print(fuj)
+                            fo = open("static/zapisovanie/databaza.txt","a+")
+                            fo.write("%s\r\n" %fuj)
+                            fo.close()
+                            cursor = db.cursor()
+                            cursor.execute("SELECT MAX(id) FROM merania")
+                            maxid = cursor.fetchone()
+                            cursor.execute("INSERT INTO merania (id, hodnoty) VALUES (%s, %s)", (maxid[0] + 1, fuj))
+                            db.commit()
+                        
+                        dataList = []
+                        dataCounter = 0
+                    if float (A) >= float (vlhkost):
+                        socketio.emit('my_response',
+                                  {'data': json.dumps({"vlhkost": kontrola}), 'count': count},
+                                  namespace='/test')
+                     
+                    socketio.emit('my_response',
+                          {'data': json.dumps({"teplota": teplota,"vlhkost": vlhkost,"argument": float (A)}), 'count': count},
+                          namespace='/test')
                     
-                    dataList = []
-                    dataCounter = 0
-                
-                socketio.emit('my_response',
-                              {'data': json.dumps({"teplota": teplota,"vlhkost": vlhkost,"argument": float (A)}), 'count': count},
-                              namespace='/test')
-                
-                socketio.emit('my_response2',
-                      {'data': teplota,'data1': vlhkost,'count': count},
-                      namespace='/test')
-                
-                socketio.emit('my_response3',
-                      {'data': teplota,'data1': vlhkost, 'count': count},
-                      namespace='/test')
+                    socketio.emit('my_response2',
+                          {'data': teplota,'data1': vlhkost,'count': count},
+                          namespace='/test')
+                    
+                    socketio.emit('my_response3',
+                          {'data': teplota,'data1': vlhkost, 'count': count},
+                          namespace='/test')
+    #                 if float(D) >= 1:
+    #                     socketio.emit('my_response4',
+    #                           {'data': float (D), 'count': count},
+    #                           namespace='/test')
             else:
                 socketio.sleep()
                 
@@ -112,6 +131,32 @@ def test_message(message):
     A = message['value']    
     emit('my_response',
          {'data': message['value'], 'count': session['receive_count']})
+
+# @socketio.on('my_event1', namespace='/test')
+# def test_message1(message):   
+#     session['receive_count'] = session.get('receive_count', 0) + 1
+#     global D
+#     D = message['value']    
+#     emit('my_response',
+#          {'data': message['value'], 'count': session['receive_count']})
+
+@socketio.on('Begin_request', namespace='/test')
+def Begin_request():
+    global b
+    print('Begin', request.sid)
+    session['receive_count'] = session.get('receive_count', 0) + 1
+    emit('my_response',
+         {'data': 'Begin!', 'count': session['receive_count']})
+    b = 1
+    
+@socketio.on('End_request', namespace='/test')
+def End_request():
+    global b
+    print('End', request.sid)
+    session['receive_count'] = session.get('receive_count', 0) + 1
+    emit('my_response',
+         {'data': 'End!', 'count': session['receive_count']})
+    b = 0
 
 @socketio.on('start_request', namespace='/test')
 def start_request():
@@ -175,3 +220,4 @@ def test_stop():
 
 if __name__ == '__main__':
     socketio.run(app, host="0.0.0.0", port=80, debug=True)
+
